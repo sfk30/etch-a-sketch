a9439cef91c3345d866d7fbe5941fa7a092b53e7
Shafia <111538877+sfk30@users.noreply.github.com>
2023-09-27 20:48:57 +0100

    Added .innerHTML = '' to reload new size grids

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 const grid = document.querySelector('#grid')
 
-for (let i = 0; i < 16; i ++) {
-    let row = document.createElement('div')
-    row.classList.add('row')
-    grid.appendChild(row)
-    for (let j = 0; j < 16; j ++) {
-        let column = document.createElement('div')
-        column.classList.add('column')
-        row.appendChild(column)
+let userSize = document.getElementById('user-size');
+let userSubmit = document.getElementById('sub-btn');
+userSubmit.addEventListener('click', makeGrid);
+userSubmit.addEventListener('click', colorMe);
+
+
+
+function makeGrid() {
+    let number = userSize.value;
+    grid.innerHTML = ''
+    for (let i = 0; i < number; i ++) {
+        let row = document.createElement('div')
+        row.classList.add('row')
+        grid.appendChild(row)
+        for (let j = 0; j < number; j ++) {
+            let column = document.createElement('div')
+            column.classList.add('column')
+            row.appendChild(column)
+        }
     }
 }
 
-const columns = document.querySelectorAll('.column')
 
-columns.forEach((column) => {
-    column.addEventListener('mouseenter', (event) => {
-        event.target.style.backgroundColor = 'purple'
+function colorMe() {
+    const columns = document.querySelectorAll('.column')
+
+    columns.forEach((column) => {
+        column.addEventListener('mouseenter', (event) => {
+            event.target.style.backgroundColor = 'purple'
+        })
     })
-})
-// columns.addEventlistener('mouseenter', (event) => {
-//     event.target.style.color = 'purple'
-// }) this does not work because columns is a nodelist
-// addEventListener works only on element not n array
+}
