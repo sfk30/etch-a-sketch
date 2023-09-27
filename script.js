9190df174f773eac60242e08d392d78e8a30db0e
Shafia <111538877+sfk30@users.noreply.github.com>
2023-09-27 12:25:48 +0100

    Corrected final line of grid layout to append column div to row div

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const grid = document.querySelector('#grid')
 
-for (i = 0; i < 16; i ++) {
+for (let i = 0; i < 16; i ++) {
     let row = document.createElement('div')
     row.classList.add('row')
     grid.appendChild(row)
-    for (j = 0; j < 16; j ++) {
+    for (let j = 0; j < 16; j ++) {
         let column = document.createElement('div')
         column.classList.add('column')
         row.appendChild(column)
